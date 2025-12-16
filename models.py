5b6bc2d0c170863fca5dbb4247a5108b9e3e475a
MaxKorjik <koshchinmaxim06@gmail.com>
2025-12-16 18:00:19 +0200

    upd models (category, stream)

--- models.py
+++ models.py
@@ -1,11 +1,37 @@
+import email
 from database import Base
-from sqlalchemy import Column, String, Text, Integer
+from sqlalchemy import Boolean, Column, ForeignKey, String, Text, Integer, DateTime
 from sqlalchemy.orm import relationship
-
+from datetime import datetime
 class User(Base):
     __tablename__ = "users"
     
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, index=True)
+    username = Column(String(255), index=True)
     password = Column(String, unique=True)
+    email = Column(String(255), unique=True, nullable=True, index=True)
     avatar_url = Column(String)
+    role = Column(String(255), nullable=True, index=True)
+    is_active = Column(Boolean)
+    created_at = Column(DateTime, server_default=datetime.now())
+    
+class Category(Base):
+    __tablename__ = "categories"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255), unique=True, index=True)
+    
+class Stream(Base):
+    __tablename__ = "streams"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    streamer_id = Column(
+        Integer,
+        ForeignKey("users.id"),
+        nullable=False
+    )
+
+    streamer = relationship(
+        "User",
+        back_populates="streams"
+    )
